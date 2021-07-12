c82fb0d92d5efa76d1b113a593839bf1121d8914
Ragul Murugesan <y509198@softwareag.com>
2021-07-12 23:30:48 +0530

    Updates

---
+++ precache-manifest.8a7656f099fa1f89850ce57d05f503f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9354f7691fd070e3c36e07081a603e",
+    "url": "/react-whatsapp/index.html"
+  },
+  {
+    "revision": "2fd3964cc86ea8d22e8e",
+    "url": "/react-whatsapp/static/css/main.8a210cad.chunk.css"
+  },
+  {
+    "revision": "022ce68c4a2a5a4ce89f",
+    "url": "/react-whatsapp/static/js/2.e6e51cc8.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/react-whatsapp/static/js/2.e6e51cc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd3964cc86ea8d22e8e",
+    "url": "/react-whatsapp/static/js/main.ab8d36f4.chunk.js"
+  },
+  {
+    "revision": "24c5d16256588cfab4e6",
+    "url": "/react-whatsapp/static/js/runtime-main.12e2389c.js"
+  }
+]);
